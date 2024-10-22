f2312ba27ae04d0f283ad721d9624a51cd112401
sviatoslavtataryn <www.t.svyatoslav55@gmail.com>
2024-10-22 23:02:48 +0200

    Fixed calculate_washing_price()

--- app/main.py
+++ app/main.py
@@ -21,9 +21,12 @@ class CarWashStation:
         return round(total_income, 1)
 
     def calculate_washing_price(self, car: Car) -> float:
-        price = car.comfort_class * (
-            self.clean_power - car.clean_mark
-        ) * self.average_rating / self.distance_from_city_center
+        clean_diff = self.clean_power - car.clean_mark
+        class_multiplier = car.comfort_class
+        rating = self.average_rating
+        distance = self.distance_from_city_center
+
+        price = class_multiplier * clean_diff * rating / distance
         return round(price, 1)
 
     def wash_single_car(self, car: Car) -> None:
